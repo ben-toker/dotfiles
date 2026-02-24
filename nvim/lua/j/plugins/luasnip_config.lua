c6ec90f61b5da1853c9f2cc513496f0668229191
ben-toker <117331544+ben-toker@users.noreply.github.com>
2026-02-23 22:28:07 -0500

    added latex snippets to luasnips and now using starship prompt

--- nvim/lua/j/plugins/luasnip_config.lua
+++ nvim/lua/j/plugins/luasnip_config.lua
@@ -1,3 +1,5 @@
+
+
 return {
   "L3MON4D3/LuaSnip",
   config = function()
@@ -5,6 +7,27 @@ return {
     local s = ls.snippet
     local t = ls.text_node
     local i = ls.insert_node
+    local i = ls.insert_node
+    local f = ls.function_node
+
+
+  local function gen_bipartite(args)
+    local m = tonumber(args[1][1]) or 1
+    local n = tonumber(args[2][1]) or 1
+    
+    local v_list = {}
+    local w_list = {}
+    
+    for j = 1, m do table.insert(v_list, "v_" .. j) end
+    for j = 1, n do table.insert(w_list, "u_" .. j) end
+    
+    local v_str = "{" .. table.concat(v_list, ", ") .. "}"
+    local w_str = "{" .. table.concat(w_list, ", ") .. "}"
+    
+    -- Returns the vertex lists for the subgraph and the connection line
+    return v_str .. " -- " .. w_str .. ";"
+end
+
 
     -- Add your graph and math snippets here
     ls.add_snippets("tex", {
@@ -15,6 +38,16 @@ return {
     t({ ";", "    };", "  \\end{tikzpicture}", "\\end{center}" }),
 }),
 
+-- Complete Bipartite Graph Snippet (K_m,n)
+ s("kmn", {
+    t({ "\\begin{center}", "  \\begin{tikzpicture}", "    \\graph [math nodes, nodes={draw, circle}, " }),
+    t("grow right="), i(5, "3cm"), t(", branch down="), i(6, "1.2cm"),
+    t({ "] {", "      subgraph K_nm [n=" }),
+    i(1, "3"), t(", m="), i(2, "3"), 
+    t(", V={"), i(3, "a"), t("_1, "), f(function(args) return args[1][1] end, {3}), t("_2, "), f(function(args) return args[1][1] end, {3}), t("_3"),
+    t("}, W={"), i(4, "b"), t("_1, "), f(function(args) return args[1][1] end, {4}), t("_2, "), f(function(args) return args[1][1] end, {4}), t("_3"),
+    t({ "}];", "    };", "  \\end{tikzpicture}", "\\end{center}" }),
+}),
       -- Matrix for Linear Algebra Grading (3x3)
       s("mat3", {
         t({ "\\begin{bmatrix}", "  " }), 
@@ -23,6 +56,11 @@ return {
         i(7), t(" & "), i(8), t(" & "), i(9), 
         t({ "", "\\end{bmatrix}" })
       }),
+
+
+
+
+
     })
   end,
 }
